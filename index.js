93b50ed5c5df392c562942116806b1cc280e9de1
ZehabFaisal <zehabfaisal2003@gmail.com>
2026-05-24 18:08:43 +0500

    Fix CORS for Vercel preview URLs

--- index.js
+++ index.js
@@ -13,7 +13,24 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
-app.use(cors());
+// app.use(cors({
+//   origin: "https://mern-auth-frontend.vercel.app"
+// }));
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) {
+        return callback(null, true);
+    } 
+    if (origin.includes('vercel.app') || origin.includes('localhost')) {
+      callback(null, true);
+    } 
+    else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
     res.send("Welcome to the User Authentication API");
@@ -23,3 +40,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
