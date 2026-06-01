780cbc7f68b87ae6e6dd35795967f631930d8d34
ZehabFaisal <zehabfaisal2003@gmail.com>
2026-06-01 17:19:43 +0500

    Updated index.js file code

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
+const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 const connectDB = require("./config/db");
 connectDB();
 
-const cors = require('cors');
 const authRoutes = require("./routes/authRoutes");
-const express = require('express');
 const app = express();
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "http://localhost:5000",
+    "https://user-auth-frontend-mocha.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
-// app.use(cors({
-//   origin: "https://mern-auth-frontend.vercel.app"
-// }));
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) {
-        return callback(null, true);
-    } 
-    if (origin.includes('vercel.app') || origin.includes('localhost')) {
-      callback(null, true);
-    } 
-    else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-
 app.get("/", (req, res) => {
-    res.send("Welcome to the User Authentication API");
+  res.send("Welcome to the User Authentication API");
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on -> http://localhost:${PORT}`);
 });
-
-module.exports = app;
