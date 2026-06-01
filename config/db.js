178c2cc106df25cfbcaa637a0be0db00ed1fd1ac
ZehabFaisal <zehabfaisal2003@gmail.com>
2026-06-01 17:18:54 +0500

    Updated config code

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+let cachedDb = null;
 
 const connect_DB = async () => {
+  if (cachedDb) {
+    return cachedDb;
+  }
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    cachedDb = db;
     console.log('Connected to MongoDB Successfully!');
+    return db;
   } 
   catch (error) {
     console.error('Error! connecting to MongoDB occurred:', error);
