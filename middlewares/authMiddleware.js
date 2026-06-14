755be9dfa4442741110c63745efe4c6f06d05628
ZehabFaisal <zehabfaisal2003@gmail.com>
2026-06-14 23:04:25 +0500

    Updated middleware file code

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -12,7 +12,7 @@ const verifyToken = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userid).select('-password');
     if (!user) {
-      return res.status(401).json({ message: "User not found, authorization denied" });
+      return res.status(401).json({ message: "Error! User not found, authorization is denied" });
     }
 
     req.user = user;
